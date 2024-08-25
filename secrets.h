58e9b751a6a0e7d07f05ee4fc8d2c8bde5228a92
Pranjal7070 <pranjal4799.be23@chitkara.edu.in>
2024-08-25 14:09:51 +0530

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Sandeep"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "sweethome"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 2635762			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "IAKAFKXS05932OYR"   // replace XYZ with your channel write API Key
